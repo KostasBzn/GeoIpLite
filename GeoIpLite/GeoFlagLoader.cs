86ba4d08dcd2408b41840da8e8ab6fe3c8e9804d
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-11-19 11:41:09 +0000

    Flag_loader_flag_resources_added

--- GeoIpLite/GeoFlagLoader.cs
+++ GeoIpLite/GeoFlagLoader.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace GeoIpLite
@@ -10,5 +7,18 @@ namespace GeoIpLite
     internal class GeoFlagLoader
     {
         public ImageList flgImgList {  get; set; }
+
+        public GeoFlagLoader()
+        {
+            flgImgList = new ImageList();
+            // original flag size 29x23px with 4px transparent margin each side
+            // the png size can be customised for the project needs - feel free
+            flgImgList.ImageSize = new Size(32, 24); 
+        }
+
+        private void AddImage(string coutryCode)
+        {
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(FlagResources));
+        }
     }
 }
