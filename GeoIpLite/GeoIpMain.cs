6b3e6c0b55ce4c07dafc2efda8217b07eb4c68e2
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-11-19 11:00:58 +0000

    initialise_flags/load_CountryDatabase

--- GeoIpLite/GeoIpMain.cs
+++ GeoIpLite/GeoIpMain.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using MaxMind.GeoIP2;
+using System.Net;
+using System.Reflection;
+using System.Windows.Forms;
 namespace GeoIpLite
 {
     public class GeoIpMain
     {
+        private DatabaseReader _databaseReader;
+        public ImageList flagImageList;
+
+        public GeoIpMain()
+        {
+            var getFlags = new GeoFlagLoader(); // we intialise the flag list
+            flagImageList = getFlags.flgImgList;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream("GeoIpLite.dbip-country-lite-2025-11.mmdb"))
+            {
+                _databaseReader = new DatabaseReader(stream);
+            }
+        }
     }
+
+    
 }
